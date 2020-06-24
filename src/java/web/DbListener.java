ea648e28a0b69abe509a49990f7284d05d32d124
Daniela Oliveira Teixeira de Sousa <daniela-lavigne@hotmail.com>
2020-06-24 11:29:15 -0300

    Implementando as perguntas do projeto anterior novamente

--- src/java/web/DbListener.java
+++ src/java/web/DbListener.java
@@ -52,9 +52,62 @@ import javax.servlet.ServletContextListener;
                     + "VALUES('Fulano da Silva', 'fulano', '"+("123456".hashCode())+"', 'USER')";
                 stmt.executeUpdate(SQL);
             }
+            step = "Table 'questions' questions";
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS question("
+                    + "description VARCHAR(250) PRIMARY KEY,"
+                    + "answer VARCHAR(250) NOT NULL,"
+                    + "answerwrong1 VARCHAR(250) NOT NULL,"
+                    + "answerwrong2 VARCHAR(250) NOT NULL,"
+                    + "answerwrong3 VARCHAR(250) NOT NULL"
+                    + ")");
+            
+            //CRIANDO PERGUNTAS E RESPOSTAS
+            step = "Default questions creation";
+            if (Question.getQuestions().isEmpty()){
+               stmt.executeUpdate("INSERT INTO questions(description, answer, answerwrong1, answerwrong2, answerwrong3) "
+                    + "VALUES('COVID-19 é a doença infecciosa causada pelo novo coronavírus:', 'SARS-COV-2', 'SARS-COV-1', 'SARS-COV-4', 'SARS-COV-3')");
+                stmt.executeUpdate("INSERT INTO questions(description, answer, answerwrong1, answerwrong2, answerwrong3) "
+                    + "VALUES('Qual a capital do Japão?','Tóquio','Pequim','Roma','Fortaleza')");
+                stmt.executeUpdate("INSERT INTO questions(description, answer, answerwrong1, answerwrong2, answerwrong3) "
+                    + "VALUES('Presidente dos Estados Unidos?', 'Donald Trump','Barack Obama','Dilma Roussef', 'Hilary Clynton')");
+                stmt.executeUpdate("INSERT INTO questions(description, answer, answerwrong1, answerwrong2, answerwrong3) "
+                    + "VALUES('Onde foi realizada as Olimpíadas de 2016?', 'Brasil', 'China','Grécia','Inglaterra')");
+                stmt.executeUpdate("INSERT INTO questions(description, answer, answerwrong1, answerwrong2, answerwrong3) "
+                    + "VALUES('Recursão é um método de programação no qual uma função pode chamar:', 'a si mesma', 'outra função', 'função identificada', 'função não identificada')");
+                stmt.executeUpdate("INSERT INTO questions(description, answer, answerwrong1, answerwrong2, answerwrong3) "
+                    + "VALUES('Quantas vezes o Brasil foi campeão mundial de futebol?', '5', '7', '2', '3')");
+                stmt.executeUpdate("INSERT INTO questions(description, answer, answerwrong1, answerwrong2, answerwrong3) "
+                    + "VALUES('Quem foi o criador do Linux?', 'Linus Torvalds', 'Steve Jobs', 'Steve Wozniak', 'Bill Gattes')");         
+                stmt.executeUpdate("INSERT INTO questions(description, answer, answerwrong1, answerwrong2, answerwrong3) "
+                    + "VALUES('Qual o significado da palavra Listener?','Ouvinte','Falante','Surdo','Mudo')");
+                stmt.executeUpdate("INSERT INTO questions(description, answer, answerwrong1, answerwrong2, answerwrong3) "
+                    + "VALUES('Qual a data da proclamação da República?', '15 de novembro', '07 de setembro', '22 de janeiro', '12 de outubro')");
+                stmt.executeUpdate("INSERT INTO questions(description, answer, answerwrong1, answerwrong2, answerwrong3) "
+                    + "VALUES('Qual a primeira capital do Brasil?', 'Rio de Janeiro', 'Brasília', 'São Paulo', 'Minas Gerais')");   stmt.executeUpdate(SQL);
+                }
+            
+            //CRIANDO TABELA DE USUARIOS
+            step = "attempt Table creation";
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS attempt("
+                    + "login VARCHAR(200) NOT NULL,"
+                    + "result NUMBER NOT NULL,"
+                    + "date DATE NOT NULL," 
+                    + "FOREIGN KEY(login) REFERENCES users(login))");
+            
+            step = "Default users creation";
+            if (User.getUsers().isEmpty()){
+            //CRIANDO USUARIOS
+                stmt.executeUpdate("INSERT INTO users(login, media, date) "
+                    + "VALUES('Jane', '10', 2020-06-19)");  
+                }
+            
+            
+            stmt.close();
+            con.close();
         }catch(Exception ex){
             exceptionMessage = step + ": " + ex.getMessage();
         }
+        
     }
 
     @Override
